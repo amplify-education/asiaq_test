564a567baed6b24ded3e9940f11c67398af81bef
Rachel Ruster <rruster@amplify.com>
2017-01-10 15:41:50 -0800

    Add volume disk usage to snapshot tags

--- disco_aws_automation/version.py
+++ disco_aws_automation/version.py
@@ -1,5 +1,5 @@
 """Place of record for the package version"""
 
-__version__ = "1.1.15"
+__version__ = "1.1.16"
 __rpm_version__ = "WILL_BE_SET_BY_RPM_BUILD"
 __git_hash__ = "WILL_BE_SET_BY_EGG_BUILD"
