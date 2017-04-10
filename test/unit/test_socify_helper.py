8a420b41247025a77328b05375b4ca17666ab133
Rachel Ruster <rruster@amplify.com>
2017-04-10 14:21:33 -0700

    AL-1128: update unit test to handle string message returned by send_event and add extra unit test to increase coverage

--- test/unit/test_socify_helper.py
+++ test/unit/test_socify_helper.py
@@ -11,12 +11,25 @@ class SocifyHelperTest(TestCase):
     """Test Socify Helper"""
     def setUp(self):
         self._soc_helper = SocifyHelper("AL-1102", False, "ExampleEvent", config=get_mock_config({
-            'socify': {'socify_baseurl': 'https://socify.aws.wgen.net/soc'}}))
+            'socify': {'socify_baseurl': 'https://socify-ci.aws.wgen.net/soc'}}))
 
     def test_build_event_url(self):
         """Test socify build event url"""
         url = self._soc_helper._build_event_url()
-        self.assertEqual(url, "https://socify.aws.wgen.net/soc/event")
+        self.assertEqual(url, "https://socify-ci.aws.wgen.net/soc/event")
+
+    def test_build_event_url_no_socify_config(self):
+        """Test socify build event url when the socify section is missing from the config"""
+        self._soc_helper = SocifyHelper("AL-1102", False, "ExampleEvent", config=get_mock_config({}))
+        url = self._soc_helper._build_event_url()
+        self.assertIsNone(url)
+
+    def test_build_event_url_no_socify_url(self):
+        """Test socify build event url when the socify section is missing from the config"""
+        self._soc_helper = SocifyHelper("AL-1102", False, "ExampleEvent", config=get_mock_config({
+            'socify': {'baseurl': 'https://socify-ci.aws.wgen.net/soc'}}))
+        url = self._soc_helper._build_event_url()
+        self.assertIsNone(url)
 
     def test_build_event_json(self):
         """Test socify build event json data"""
@@ -32,28 +45,37 @@ class SocifyHelperTest(TestCase):
     def test_send_event(self):
         """Test send event with no error"""
         res = self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, msg="test was successfull")
-        self.assertEqual(res, {"message": "SOCIFY has successfully processed the event: ExampleEvent"})
+        self.assertEqual(res, "SOCIFY has successfully processed the event: ExampleEvent")
 
     def test_send_event_soc_bad_data(self):
         """Test send event with SOC BAD DATA"""
         res = self._soc_helper.send_event(SocifyHelper.SOC_EVENT_BAD_DATA, err_msg="No AMI")
-        self.assertEqual(res, {"message": "SOCIFY has successfully processed the event: ExampleEvent"})
+        self.assertEqual(res, "SOCIFY has successfully processed the event: ExampleEvent")
 
     def test_send_event_soc_error(self):
         """Test send event with SOC ERROR"""
         res = self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, hostclass="myhostclass",
                                           err_msg="some Error occured")
-        self.assertEqual(res, {"message": "SOCIFY has successfully processed the event: ExampleEvent"})
+        self.assertEqual(res, "SOCIFY has successfully processed the event: ExampleEvent")
 
     def test_send_event_invalid_ticket(self):
         """Test send event using an invalid Ticket ID"""
         self._soc_helper._ticket_id = "AL-110266"
-        with self.assertRaisesRegexp(RuntimeError, '{"errorMessage": "SOCIFY failed executing the event: '):
-            self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, hostclass="myhostclass",
-                                        msg="test was successfull")
+        expected_res = "SOCIFY failed executing the event: Failure while processing Socify event " \
+                       "ExampleEvent: Issue does not exist or you do not have permission to see it."
+        res = self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, hostclass="myhostclass",
+                                          msg="test was successfull")
+        self.assertEquals(res, expected_res)
 
     def test_send_event_invalid_url(self):
         """Test send event using an invalid URL"""
-        self._soc_helper._socify_url = "https://socify.aws.wgen.com/soc/events"
-        with self.assertRaisesRegexp(RuntimeError, 'Failure sending event to Socify:'):
-            self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, hostclass="myhostclass")
+        self._soc_helper._socify_url = "https://socify-ci.aws.wgen.com/soc/events"
+        expected_res = 'Failed sending the Socify event'
+        res = self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, hostclass="myhostclass")
+        self.assertEquals(res, expected_res)
+
+    def test_send_event_no_url(self):
+        """Test send event with no error"""
+        self._soc_helper = SocifyHelper("AL-1102", False, "ExampleEvent", config=get_mock_config({}))
+        res = self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, msg="test was successfull")
+        self.assertIsNone(res)
