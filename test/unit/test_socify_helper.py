a3c8ea4e16df0711aa3ce6b2fdc986fa7f6e5432
Rachel Ruster <rruster@amplify.com>
2017-04-14 14:20:52 -0700

    AL-1128 Fix unit test and remove useless test case. Also added new unit test to increase coverage. - Mock requests calls using request_mock instead of sending real http requests - Added test for case where exception returned - Added test for case where a 400 error code is returned

--- test/unit/test_socify_helper.py
+++ test/unit/test_socify_helper.py
@@ -1,10 +1,14 @@
 """
 Tests Socify Helper
 """
-
 from unittest import TestCase
+import requests
+import requests_mock
+
 from disco_aws_automation.socify_helper import SocifyHelper
 from test.helpers.patch_disco_aws import get_mock_config
+
+SOCIFY_API = 'https://socify-ci.aws.wgen.net/soc/event'
 
 
 class SocifyHelperTest(TestCase):
@@ -42,37 +46,45 @@ class SocifyHelperTest(TestCase):
                              "msg": "test was successfull"}}
         self.assertEqual(data, res_data)
 
-    def test_send_event(self):
+    def test_build_event_json_with_sub_command(self):
+        """Test socify build event json data"""
+        self._soc_helper._sub_command = 'mySubCommand'
+        data = self._soc_helper._build_event_json(SocifyHelper.SOC_EVENT_OK, hostclass="myhostclass",
+                                                  msg="test was successfull")
+        res_data = {"ticketId": "AL-1102",
+                    "cmd": "ExampleEvent",
+                    "data": {"status": SocifyHelper.SOC_EVENT_OK,
+                             "sub_cmd": "mySubCommand",
+                             "hostclass": "myhostclass",
+                             "msg": "test was successfull"}}
+        self.assertEqual(data, res_data)
+
+    @requests_mock.Mocker()
+    def test_send_event(self, mock_requests):
         """Test send event with no error"""
-        res = self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, msg="test was successfull")
-        self.assertEqual(res, "SOCIFY has successfully processed the event: ExampleEvent")
+        mock_response = {
+            'message': 'SOCIFY has successfully processed the event: ExampleEvent'
+        }
+        mock_requests.post(SOCIFY_API, json=mock_response)
+        self.assertEqual(self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, msg="test was successfull"),
+                         "SOCIFY has successfully processed the event: ExampleEvent")
 
-    def test_send_event_soc_bad_data(self):
-        """Test send event with SOC BAD DATA"""
-        res = self._soc_helper.send_event(SocifyHelper.SOC_EVENT_BAD_DATA, err_msg="No AMI")
-        self.assertEqual(res, "SOCIFY has successfully processed the event: ExampleEvent")
+    @requests_mock.Mocker()
+    def test_send_event_timeout(self, mock_requests):
+        """Test send event with no error"""
+        mock_requests.post(SOCIFY_API, exc=requests.exceptions.ConnectTimeout)
+        self.assertEqual(self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, msg="test was successfull"),
+                         "Failed sending the Socify event")
 
-    def test_send_event_soc_error(self):
-        """Test send event with SOC ERROR"""
-        res = self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, hostclass="myhostclass",
-                                          err_msg="some Error occured")
-        self.assertEqual(res, "SOCIFY has successfully processed the event: ExampleEvent")
-
-    def test_send_event_invalid_ticket(self):
-        """Test send event using an invalid Ticket ID"""
-        self._soc_helper._ticket_id = "AL-110266"
-        expected_res = "SOCIFY failed executing the event: Failure while processing Socify event " \
-                       "ExampleEvent: Issue does not exist or you do not have permission to see it."
-        res = self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, hostclass="myhostclass",
-                                          msg="test was successfull")
-        self.assertEquals(res, expected_res)
-
-    def test_send_event_invalid_url(self):
-        """Test send event using an invalid URL"""
-        self._soc_helper._socify_url = "https://socify-ci.aws.wgen.com/soc/events"
-        expected_res = 'Failed sending the Socify event'
-        res = self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, hostclass="myhostclass")
-        self.assertEquals(res, expected_res)
+    @requests_mock.Mocker()
+    def test_send_event_httperror(self, mock_requests):
+        """Test send event with no error"""
+        mock_response = {
+            'errorMessage': 'SOCIFY failed executing the event request'
+        }
+        mock_requests.post(SOCIFY_API, json=mock_response, status_code=400)
+        self.assertEqual(self._soc_helper.send_event(SocifyHelper.SOC_EVENT_OK, msg="test was successfull"),
+                         "SOCIFY failed executing the event request")
 
     def test_send_event_no_url(self):
         """Test send event with no error"""
