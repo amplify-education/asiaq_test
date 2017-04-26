675c5108f5774b33db84f576154ecd2be1f314fc
Johnathan Constance <jconstance@amplify.com>
2017-04-26 16:04:33 -0400

    Fix double logging in some cases

--- disco_aws_automation/disco_logging.py
+++ disco_aws_automation/disco_logging.py
@@ -6,7 +6,6 @@ import sys
 def configure_logging(debug, silent=False):
     '''Sets the default logger and the boto logger to appropriate levels of chattiness.'''
     logger = logging.getLogger('')
-
     boto_logger = logging.getLogger('boto')
     botocore_logger = logging.getLogger('botocore')
 
@@ -23,6 +22,11 @@ def configure_logging(debug, silent=False):
         logger.setLevel(logging.INFO)
         boto_logger.setLevel(logging.CRITICAL)
         botocore_logger.setLevel(logging.CRITICAL)
+
+    # If there are any handlers on the root logger, remove them so that if this function is called more
+    # than once, we don't get the same statement logged multiple times.
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
 
     stream_handler = logging.StreamHandler(sys.__stdout__)
     stream_handler.setLevel(logging.DEBUG)
