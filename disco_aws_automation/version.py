8998138d9fcf65d53ed103ad6c87810e50f06e13
Johnathan Constance <jconstance@amplify.com>
2019-04-08 11:21:47 -0400

    Version bump

--- disco_aws_automation/version.py
+++ disco_aws_automation/version.py
@@ -1,5 +1,5 @@
 """Place of record for the package version"""
 
-__version__ = "2.4.21"
+__version__ = "2.4.22"
 __rpm_version__ = "WILL_BE_SET_BY_RPM_BUILD"
 __git_hash__ = "WILL_BE_SET_BY_EGG_BUILD"
