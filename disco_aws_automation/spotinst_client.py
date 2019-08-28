1788caeab5c8190e7067f3f8e66ca994cde4d4ec
Darius Cox <dcox@amplify.com>
2019-08-28 18:05:43 -0400

    tested

--- disco_aws_automation/spotinst_client.py
+++ disco_aws_automation/spotinst_client.py
@@ -3,9 +3,10 @@ import logging
 import requests
 from requests.exceptions import Timeout, ConnectionError
 
-from .disco_config import read_config
 from disco_aws_automation.exceptions import SpotinstApiException, SpotinstRateExceededException
 from disco_aws_automation.resource_helper import Jitter
+
+from .disco_config import read_config
 
 SPOTINST_API_HOST = 'https://api.spotinst.io'
 logger = logging.getLogger(__name__)
@@ -14,9 +15,9 @@ logger = logging.getLogger(__name__)
 class SpotinstClient(object):
     """A client for the Spotinst REST API"""
 
-    def __init__(self, token, environment_name):
+    def __init__(self, token, environment_name, config_aws=None):
         self.token = token
-        self.config_aws = read_config(environment=environment_name)
+        self.config_aws = config_aws or read_config(environment=environment_name)
         self.account_id = self.config_aws.get_asiaq_option(
             option='spotinst_account_id',
             environment=environment_name
--- tests/unit/test_spotinst_client.py
+++ tests/unit/test_spotinst_client.py
@@ -7,16 +7,29 @@ from requests.exceptions import ReadTimeout, ConnectTimeout, ConnectionError
 
 from disco_aws_automation.exceptions import SpotinstRateExceededException
 from disco_aws_automation.spotinst_client import SpotinstClient
+from tests.helpers.patch_disco_aws import get_mock_config
+
+MOCK_AWS_CONFIG_DEFINITION = {
+    "disco_aws": {
+        "default_spotinst_account_id": "fake-id",
+        "default_environment": "fake-ci",
+    }
+}
 
 
 class DiscoSpotinstClientTests(TestCase):
     """Test SpotinstClient class"""
 
     # @patch('disco_aws_automation.spotinst_client.read_config')
-    def setUp(self, config_mock):
+    def setUp(self):
         """Pre-test setup"""
         # config_mock.get_asiaq_option.return_value = "fake_account_id"
-    #    self.spotinst_client = SpotinstClient(token="fooabcd", environment="fakeenvironment")
+        config_aws = get_mock_config(MOCK_AWS_CONFIG_DEFINITION)
+        self.spotinst_client = SpotinstClient(
+            token="fooabcd",
+            environment_name="fakeenvironment",
+            config_aws=config_aws
+        )
 
     @requests_mock.mock()
     def test_create_group(self, requests):
