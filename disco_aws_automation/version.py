dcf71abf812fa6028aa13f58f0cd00395df804b4
Darius Cox <dcox@amplify.com>
2019-09-27 17:13:39 -0400

    version

--- disco_aws_automation/version.py
+++ disco_aws_automation/version.py
@@ -1,5 +1,5 @@
 """Place of record for the package version"""
 
-__version__ = "2.4.26"
+__version__ = "2.4.27"
 __rpm_version__ = "WILL_BE_SET_BY_RPM_BUILD"
 __git_hash__ = "WILL_BE_SET_BY_EGG_BUILD"
