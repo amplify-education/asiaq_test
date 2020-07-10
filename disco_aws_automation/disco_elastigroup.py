8424eef71a801175d03b821bfeac2dacc5785223
Calvin Lin <calin@calin-mac2.local>
2020-07-10 09:39:26 -0400

    Revert "Bugfix get all subnets under subnetIds"
    This reverts commit 1745ee5daae7f86f42a97c520697aac864cfb822.

--- disco_aws_automation/disco_elastigroup.py
+++ disco_aws_automation/disco_elastigroup.py
@@ -88,7 +88,7 @@ class DiscoElastigroup(BaseGroup):
                 'launch_config_name': None,
                 'termination_policies': [],
                 'vpc_zone_identifier': ','.join(
-                    subnet for subnets in group['compute']['availabilityZones'] for subnet in subnets['subnetIds']
+                    zone['subnetId'] for zone in group['compute']['availabilityZones']
                 ),
                 'load_balancers': [elb['name'] for elb in load_balancer_configs
                                    if elb['type'] == 'CLASSIC'],
