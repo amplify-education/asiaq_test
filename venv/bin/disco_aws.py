473c847667309aa5e7f99912738077966d40c848
Calvin Lin <calin@calin-mac2.local>
2021-02-16 15:15:34 -0500

    cleanup

--- venv/bin/disco_aws.py
+++
@@ -1,7 +0,0 @@
-#!/Users/calin/Projects/asiaq/venv/bin/python
-# EASY-INSTALL-DEV-SCRIPT: 'asiaq==2.4.42','disco_aws.py'
-__requires__ = 'asiaq==2.4.42'
-__import__('pkg_resources').require('asiaq==2.4.42')
-__file__ = '/Users/calin/Projects/asiaq/bin/disco_aws.py'
-with open(__file__) as f:
-    exec(compile(f.read(), __file__, 'exec'))
