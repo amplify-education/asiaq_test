059b5db0768048d4e11fc012e1720213ba365538
Keli Bezhani <keli.bezhani@gmail.com>
2021-03-02 12:12:41 -0500

    Add file handler for debug logs

--- disco_aws_automation/disco_logging.py
+++ disco_aws_automation/disco_logging.py
@@ -1,5 +1,6 @@
 '''Utility function for logging'''
 import logging
+import logging.handlers as handlers
 import sys
 
 
@@ -8,6 +9,11 @@ def configure_logging(debug, silent=False):
     logger = logging.getLogger('')
     boto_logger = logging.getLogger('boto')
     botocore_logger = logging.getLogger('botocore')
+
+    # If there are any handlers on the root logger, remove them so that if this function is called more
+    # than once, we don't get the same statement logged multiple times.
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
 
     if silent and debug:
         raise Exception('Debug and silent logging options are mutually exclusive')
@@ -18,17 +24,18 @@ def configure_logging(debug, silent=False):
         logger.setLevel(logging.DEBUG)
         boto_logger.setLevel(logging.INFO)
         botocore_logger.setLevel(logging.DEBUG)
+        file_handler = handlers.RotatingFileHandler('debug.log', maxBytes=1048576, backupCount=1)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s'))
+        logger.addHandler(file_handler)
+        boto_logger.addHandler(file_handler)
+        botocore_logger.addHandler(file_handler)
     else:
         logger.setLevel(logging.INFO)
         boto_logger.setLevel(logging.CRITICAL)
         botocore_logger.setLevel(logging.CRITICAL)
 
-    # If there are any handlers on the root logger, remove them so that if this function is called more
-    # than once, we don't get the same statement logged multiple times.
-    for handler in logger.handlers:
-        logger.removeHandler(handler)
-
     stream_handler = logging.StreamHandler(sys.__stdout__)
-    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setLevel(logging.INFO)
     stream_handler.setFormatter(logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s'))
     logger.addHandler(stream_handler)
